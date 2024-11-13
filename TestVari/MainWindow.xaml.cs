bcd467725f06963972685c626759d24a09f6237e
Stefano Mora <stefano.mora@newdep.com>
2024-11-13 11:54:31 +0100

    Aggiungere i file di progetto.

---
+++ TestVari/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using TorneoSB;
+
+namespace TestVari
+{
+	/// <summary>
+	/// Logica di interazione per MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		static HttpStatusCode statusCode;
+		public static HttpStatusCode StatusCode { get { return statusCode; } }
+
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			List<Torneo> tornei = GetTornei();
+			Console.WriteLine($"Trovati {tornei.Count} tornei");
+		}
+
+
+		/// <summary>
+		/// Inizializzazione valori comuni alle chiamate REST
+		/// </summary>
+		/// <returns></returns>
+		static private RestClient InitRest()
+		{
+			var options = new RestClientOptions("https://www.salabaganzabc.com/Torneo/1.1/")
+			{
+				Authenticator = new HttpBasicAuthenticator("username", "password"),
+				ThrowOnDeserializationError = true,
+				FailOnDeserializationError = true,
+			};
+			return new RestClient(options);
+		}
+
+
+
+		/// <summary>
+		/// Recupera la lista dei tornei in DB
+		/// </summary>
+		static public List<Torneo> GetTornei()
+		{
+			var client = InitRest();
+			var request = new RestRequest("GetTornei.php", Method.Get);
+
+			List<Torneo> list = new List<Torneo>();
+			try
+			{
+				var response = client.ExecuteGet<TorneiQuery>(request);
+				Debug.WriteLine(response.Content);
+				Debug.WriteLine(response.StatusCode);
+				if (response.StatusCode == HttpStatusCode.OK)
+				{
+					if (response.Data != null)
+						foreach (var item in response.Data.list)
+						{
+							Torneo t = Torneo.FromData(item);
+							list.Add(t);
+						};
+				}
+
+				statusCode = response.StatusCode;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+			return list;
+		}
+
+
+
+	}
+}
---
+++ TestVari/Torneo.cs
@@ -0,0 +1,54 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+
+namespace TorneoSB
+{
+	public class Torneo
+	{
+		// I membri devono essere 'proprietà' per consentire il parsing JSON
+
+		public string id { get; set; }		// Da query arriva una stringa
+		public string nome { get; set; }
+		public DateTime dataDa {  get; set; }
+		public DateTime dataA { get; set; }
+
+
+		/// <summary>
+		/// Factory di conversione
+		/// </summary>
+		/// <param name="query"></param>
+		/// <returns></returns>
+		internal static Torneo FromData( TorneoQuery query)
+		{
+			Torneo torneo = new Torneo();
+			torneo.id = query.id;
+			torneo.nome = query.nome;
+
+			DateTime tmp;
+			bool parsed = DateTime.TryParse(query.dataIniziale, out tmp);
+			torneo.dataDa = parsed ? tmp : new DateTime(2020, 01, 01);
+			parsed = DateTime.TryParse(query.dataFinale, out tmp);
+			torneo.dataA = parsed ? tmp : new DateTime(2020, 01, 01);
+			return torneo;
+		}
+
+	}
+
+
+	public class TorneoQuery
+	{
+		public string id { get; set; }
+		public string nome { get; set; }
+		public string dataIniziale { get; set; }
+		public string dataFinale { get; set; }
+	}
+
+	class TorneiQuery
+	{
+		public string ret { get; set; }
+		public List<TorneoQuery> list { get; set; }
+	}
+
+}
